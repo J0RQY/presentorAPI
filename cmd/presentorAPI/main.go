bf17d8e756584d6467ce9b308496b530cd19529f
Jordy <73104378+J0RQY@users.noreply.github.com>
2025-10-09 19:58:43 +0200

    Features/routing (#2)
    * feat: Added an setup for routing
    
    * FEAT: Added codeowners file

--- cmd/presentorAPI/main.go
+++ cmd/presentorAPI/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/j0rqy/presentorAPI/internal/routes"
+)
 
+func main() {
+	r := gin.Default()
+	routes.Routes(r)
+	r.Run()
 }
